64b18d908d0eac9fc9149bbc38ebd0c547a450de
Daniel Vanik <vanik.daniel97@gmail.com>
2019-05-10 00:21:36 +0200

    pridanie PHPMail do priecinka uloha3

---
+++ uloha3/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Framework</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-light navbar-custom navbar-expand-lg">
+        <a class="navbar-brand" href="./index.php">
+            <i class="material-icons nav-icon pt-2">home</i>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="./login.php">Úloha 1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./history.php">Úloha 2</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="uloha3/index.php">Úloha 3</a>
+                </li>
+            </ul>
+                <span class="navbar-text text-right text-white">
+                        Username :
+                </span>
+                <a href="javascript:;"><i class="material-icons nav-icon pt-2">exit_to_app</i></a>
+        </div>
+    </nav>
+</header>
+
+<div class="container mt-3">
+    <div class="container-fluid">
+        <div class="col-sm-12 text-center" style="margin-top: 33%">
+            <a href="../general.admin/index.php" class="btn btn-primary">Admin</a>
+        </div>
+    </div>
+</div>
+
+<footer class="footer text-center fixed-bottom navbar-custom" style="height: 50px;">
+    <span class="text-white pd-top">Developed by : LR, DV, MM, SR, MR</span>
+</footer>
+</body>
+</html>
